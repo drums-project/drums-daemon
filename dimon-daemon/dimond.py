941b0ae2999a73e1ee96c3728ac48a7907442bdd
Mani Monajjemi <mani.monajjemi@gmail.com>
2013-10-11 13:55:23 -0700

    The ability to apply filters to data that are being streamed added. Perliminary results. Not stable yet.

--- dimon-daemon/dimond.py
+++ dimon-daemon/dimond.py
@@ -15,6 +15,7 @@ from copy import copy
 import bottle
 import zmq
 import msgpack
+import json
 
 from dimon import Dimon, DimonError
 from pprint import pprint
@@ -89,7 +90,7 @@ class MsgpackSafeCache:
 
                     d = cached['data']
                     try:
-                        d = reduce(lambda d, key: d.get(key, None), path, d)
+                        d = reduce(lambda di, key: di.get(key, None), path, d)
                     except AttributeError:
                         d = None
 
@@ -100,6 +101,112 @@ class MsgpackSafeCache:
                         return None
 
 
+class Filter(object):
+    def __init__(self):
+        self.tree = dict()
+
+    def __str__(self):
+        return json.dumps(self.tree)
+
+    def __search_keys(self, _dict, _list):
+        ret_l = []
+        for l in _list:
+            if str(l) in _dict:
+                ret_l.append(str(l))
+        return ret_l
+
+    def add(self, _type, _key, key_path):
+        key = str(_key)
+        print "Request for %s:%s:%s" % (_type, key, key_path)
+        # for test only
+        path = key_path.split('/')
+        if len(path) > 7 or len(path) < 1:
+            return False
+
+        if not _type in self.tree:
+            self.tree[_type] = dict()
+
+        # TODO: Check if key is valid
+        if not key in self.tree[_type]:
+            # sets are not JSON serializable!
+            self.tree[_type][key] = dict()
+
+        # Store the string only
+        if not key_path in self.tree[_type][key]:
+            self.tree[_type][key][key_path] = True
+
+        pprint(self.tree)
+        return True
+
+    def remove(self, _type, key, key_path):
+        try:
+            del self.tree[_type][key][key_path]
+            if not self.tree[_type][key]:
+                del self.tree[_type][key]
+            if not self.tree[_type]:
+                del self.tree[_type]
+
+        except KeyError:
+            return False
+
+    def apply(self, data):
+        # The empty tree -> no filters
+        if not self.tree:
+            return data
+
+        types = self.__search_keys(self.tree, [data['type'], '~'])
+        print "Matched Types: %s" % types
+        if not types:
+            return None
+
+        keys = {}
+        for _type in types:
+            keys[_type] = self.__search_keys(self.tree[_type], [data['key'], '~'])
+
+        print "Matched Keys: %s" % keys
+        if not keys:
+            return None
+
+        ret = {}
+        ret['type'] = data['type']
+        ret['key'] = data['key']
+        ret['timestamp'] = data['data'].get('timestamp', 'nan')
+        ret['data'] = {}
+        # Save the root pointer
+        ret_root = ret
+        for _type in types:
+            for key in keys[_type]:
+                for key_path in self.tree[_type][key].keys():
+                    path = key_path.split('/')
+                    print "Checking Path %s" % path
+                    try:
+                        # This is non-destructive
+                        if path[0]:
+                            d = reduce(lambda di, key: di.get(key, None), path, data['data'])
+                        else:
+                            d = data['data']
+                    except AttributeError:
+                        d = None
+
+                    # Path is valid
+                    # `if d` will return false for values equal to 0, nasty bug
+                    if not d is None:
+                        # Traverse back to root
+                        ret = ret_root['data']
+                        print "Path is valid"
+                        for p in path:
+                            print "%s/" % p
+                            if not p in ret:
+                                ret[p] = dict()
+                            prev = ret
+                            ret = ret[p]
+                        prev[p] = d
+
+
+        if ret_root['data']:
+            return ret_root
+        else:
+            return None
 
 class DimonDaemon(object):
     def __init__(self, config = dict(), debug = None):
@@ -120,6 +227,7 @@ class DimonDaemon(object):
         self.zmq_addr = "tcp://*:" + str(self.config.get('publish_port', 8002))
         self.sock.bind(self.zmq_addr)
         self.loop_counter = 0
+        self.data_filter = Filter()
 
         self.__host_regex = re.compile("(?=^.{1,254}$)(^(?:(?!\d|-)[a-zA-Z0-9\-]{1,63}(?<!-)\.?)+(?:[a-zA-Z]{2,})$)")
         self.__ip_regex = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
@@ -144,14 +252,20 @@ class DimonDaemon(object):
 
     def _callback_pid(self, pid, data):
         #pprint(data)
-        d = msgpack.dumps({'type': 'pid', 'key' : pid, 'data' : data})
-        self.sock.send(d)
+        d = {'type': 'pid', 'key' : pid, 'data' : data}
         self.cache_pid.put('pid', pid, d)
+        d_filtered = self.data_filter.apply(d)
+        if d_filtered:
+            self.sock.send(msgpack.dumps(d_filtered))
+
 
     def _callback_host(self, host, data):
-        d = msgpack.dumps({'type': 'host', 'key' : 'host', 'data' : data})
-        self.sock.send(d)
+        d = {'type': 'host', 'key' : 'host', 'data' : data}
         self.cache_host.put('host', 'host', d)
+        d_filtered = self.data_filter.apply(d)
+        if d_filtered:
+            self.sock.send(msgpack.dumps(d_filtered))
+
 
     def _callback_latency(self, target, data):
         d = msgpack.dumps({'type': 'latency', 'key' : target, 'data' : data})
@@ -165,6 +279,17 @@ class DimonDaemon(object):
         self.cache_socket.put('socket', 'socket', d)
 
     # These are called in Bottle thread's context
+    def add_filter(self, kind, key, key_path):
+        if (self.data_filter.add(kind, key, key_path)):
+            return http_response(DimonError.SUCCESS)
+        else:
+            return http_response(DimonError.RUNTIME)
+
+    def remove_filter(self, kind, key, key_path):
+        if (self.data_filter.remove(kind, key, key_path)):
+            return http_response(DimonError.SUCCESS)
+        else:
+            return http_response(DimonError.NOTFOUND)
     def get_info(self):
         return {"name": "Dimon Daemon",
                 "version": __version__,
@@ -180,6 +305,12 @@ class DimonDaemon(object):
         self.cache_pid.remove_key('pid', pid)
         http_response(self.dimon.remove_pid(pid))
 
+    def add_filter_pid(self, pid, key_path):
+        return self.add_filter('pid', pid, key_path)
+
+    def remove_filter_pid(self, pid, key_path):
+        return self.remove_filter('pid', pid, key_path)
+
     def get_pid(self, pid, key_path = None):
         d = self.cache_pid.get('pid', pid, key_path)
         if d:
@@ -192,6 +323,12 @@ class DimonDaemon(object):
 
     def disable_host(self):
         http_response(self.dimon.remove_host())
+
+    def add_filter_host(self, key_path):
+        return self.add_filter('host', 'host', key_path)
+
+    def remove_filter_host(self, key_path):
+        return self.remove_filter('host', 'host', key_path)
 
     def get_host(self, key_path = None):
         d = self.cache_host.get('host', 'host', key_path)
@@ -243,6 +380,11 @@ class DimonDaemon(object):
         else:
             http_response(DimonError.NOTFOUND)
 
+    def get_filters(self):
+        return str(self.data_filter)
+
+
+
 if __name__ == "__main__":
     config = dict()
 
@@ -252,12 +394,13 @@ if __name__ == "__main__":
     # TODO: API version should be static for each call, not from __api__
     rp = "/dimon/v/%s" % (__api__,)
 
-    path_pid_base = rp + "/monitor/pid"
-    path_pid = path_pid_base + "/<pid:int>"
-    path_host = rp + "/monitor/host"
-    path_latency_base = rp + "/monitor/latency"
+    path_pid_base = rp + "/%s/pid"
+    path_pid_monitor = (path_pid_base % 'monitor') + "/<pid:int>"
+    path_pid_filter = (path_pid_base % 'filter') + "/<pid:re:[0-9]+|~>"
+    path_host = rp + "/%s/host"
+    path_latency_base = rp + "/%s/latency"
     path_latency = path_latency_base + "/<target>"
-    path_socket_base = rp + "/monitor/socket"
+    path_socket_base = rp + "/%s/socket"
     path_socket = path_socket_base + "/<proto:re:tcp|udp>/<direction:re:bi|src|dst>/<port:int>"
 
     logging.info("Starting dimon-daemon.")
@@ -267,26 +410,34 @@ if __name__ == "__main__":
     bottle.debug(True)
     bottle.route(rp + "/info", "GET", app.get_info)
 
-    bottle.route(path_pid, "POST", app.add_pid)
-    bottle.route(path_pid, "DELETE", app.remove_pid)
-    bottle.route(path_pid, "GET", app.get_pid)
-    bottle.route(path_pid + "/<key_path:path>", "GET", app.get_pid)
+    bottle.route(path_pid_monitor, "POST", app.add_pid)
+    bottle.route(path_pid_monitor, "DELETE", app.remove_pid)
+    bottle.route(path_pid_monitor, "GET", app.get_pid)
+    bottle.route(path_pid_monitor + "/<key_path:path>", "GET", app.get_pid)
+    bottle.route(path_pid_filter + "/<key_path:path>", "POST", app.add_filter_pid)
+    bottle.route(path_pid_filter + "/<key_path:path>", "DELETE", app.remove_filter_pid)
 
-    bottle.route(path_host, "POST", app.enable_host)
-    bottle.route(path_host, "DELETE", app.disable_host)
-    bottle.route(path_host, "GET", app.get_host)
-    bottle.route(path_host + "/<key_path:path>", "GET", app.get_host)
+    bottle.route(path_host % 'monitor', "POST", app.enable_host)
+    bottle.route(path_host % 'monitor', "DELETE", app.disable_host)
+    bottle.route(path_host % 'monitor', "GET", app.get_host)
+    bottle.route((path_host  % 'monitor') + "/<key_path:path>", "GET", app.get_host)
+    bottle.route((path_host  % 'filter') + "/<key_path:path>", "POST", app.add_filter_host)
+    bottle.route((path_host  % 'filter') + "/<key_path:path>", "DELETE", app.remove_filter_host)
 
-    bottle.route(path_latency, "POST", app.add_latency)
-    bottle.route(path_latency, "DELETE", app.remove_latency)
-    bottle.route(path_latency, "GET", app.get_latency)
-    bottle.route(path_latency + "/<key_path:path>", "GET", app.get_latency)
+    bottle.route(path_latency  % 'monitor', "POST", app.add_latency)
+    bottle.route(path_latency % 'monitor', "DELETE", app.remove_latency)
+    bottle.route(path_latency % 'monitor', "GET", app.get_latency)
+    bottle.route((path_latency % 'monitor') + "/<key_path:path>", "GET", app.get_latency)
+
+    bottle.route(path_socket % 'monitor', "POST", app.add_socket)
+    bottle.route(path_socket % 'monitor', "DELETE", app.remove_socket)
+    bottle.route(path_socket_base % 'monitor', "GET", app.get_socket)
+    bottle.route((path_socket_base % 'monitor') + "/<key_path:path>", "GET", app.get_socket)
 
 
-    bottle.route(path_socket, "POST", app.add_socket)
-    bottle.route(path_socket, "DELETE", app.remove_socket)
-    bottle.route(path_socket_base, "GET", app.get_socket)
-    bottle.route(path_socket_base + "/<key_path:path>", "GET", app.get_socket)
+    #bottle.route(path_filter_base, "GET", app.get_filters)
+    #bottle.route(path_filter, "POST", app.add_filter)
+    #bottle.route(path_filter, "DELETE", app.remove_filter)
 
     server = Thread(target = bottle.run, kwargs = {'host': config.get("host", "0.0.0.0"), 'port': config.get("port", 8001)})
     server.daemon = True;
