fbf0c3f6f8a7dfb1c285f8acd2fdf4741ac1d403
Mani Monajjemi <mani.monajjemi@gmail.com>
2013-12-10 12:22:48 -0800

    Fixed crashing bug when a process dies

--- dimon/dimon/_process.py
+++ dimon/dimon/_process.py
@@ -8,8 +8,11 @@ from _common import *
 import psutil
 
 # TODO: Filter out input pids for positive integers only
+
+
 class ProcessMonitor(TaskBase):
-    def __init__(self, result_queue, default_interval, name = "", fields = [], pids = []):
+    def __init__(self, result_queue, default_interval, name = "",
+        fields = [], pids = []):
         TaskBase.__init__(self, result_queue, default_interval, name)
         self.set_fields(fields)
         for p in list(set(pids)):
@@ -20,8 +23,7 @@ class ProcessMonitor(TaskBase):
         if fields:
             self.fields = list(set(fields))
         else:
-            self.fields = ['name', 'status','get_cpu_percent', 'get_cpu_times',
-            'get_memory_info','get_io_counters', 'get_threads']
+            self.fields = ['name', 'status', 'get_cpu_percent', 'get_cpu_times','get_memory_info', 'get_io_counters', 'get_threads']
 
     def register_task_core(self, task):
         """
@@ -51,43 +53,43 @@ class ProcessMonitor(TaskBase):
 
     def do(self):
         data = dict()
-        for pid, proc in self.task_map.items():
-            data[pid] = dict()
-            for f in self.fields:
-                attr = getattr(proc, f, None)
-                try:
+        try:
+            for pid, proc in self.task_map.items():
+                data[pid] = dict()
+                for f in self.fields:
+                    attr = getattr(proc, f, None)
                     if callable(attr):
                         if f == "get_cpu_percent":
                             dummy = attr(0)
                         else:
                             dummy = attr()
-                    elif attr != None:
+                    elif attr is not None:
                         dummy = str(attr)
                     else:
                         logging.warning("[in %s] Attribute `%s` not found."
                             % (self, f))
                         continue
-                # TODO: Fix the following circular loople
-                except psutil.error.NoSuchProcess:
-                    logging.warning("NoSuchProcess for %s, removing it", pid)
-                    del self.task_map[pid]
-                except psutil.error.AccessDenied:
-                    logging.warning("AccessDenied for %s, removing it", pid)
-                    del self.task_map[pid]
 
-                # This is all about the s**t about pickle is not able
-                # to encode/decode a nested class (used by psutils)
-                # this code converts namedtuples to dict
-                data[pid][f] = psutil_convert(dummy)
+                    # This is all about the s**t about pickle is not able
+                    # to encode/decode a nested class (used by psutils)
+                    # this code converts namedtuples to dict
+                    data[pid][f] = psutil_convert(dummy)
 
-            if data:
-                try:
-                    data[pid]['timestamp'] = time.time()
-                    self.result_queue.put(data)
-                except Queue.Full:
-                    logging.error("[in %s] Output queue is full in"
-                        % (self, ))
-                finally:
-                    pass#pprint(data)
+                if data:
+                    try:
+                        data[pid]['timestamp'] = time.time()
+                        self.result_queue.put(data)
+                    except Queue.Full:
+                        logging.error("[in %s] Output queue is full in"
+                            % (self, ))
+                    finally:
+                        pass
+        # TODO: Fix the following circular loople
+        except psutil.error.NoSuchProcess:
+            logging.warning("NoSuchProcess for %s, removing it", pid)
+            del self.task_map[pid]
+        except psutil.error.AccessDenied:
+            logging.warning("AccessDenied for %s, removing it", pid)
+            del self.task_map[pid]
 
 
